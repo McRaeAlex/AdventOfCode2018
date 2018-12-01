29b6786a726b7aa2b512008c51385b9af66c1162
Alexander McRae <alex@alexandermcrae.com>
2018-11-30 23:10:12 -0800

    Finished day one in python

---
+++ day1/Main.py
@@ -0,0 +1,41 @@
+import sys
+
+
+
+def part2():
+    lines = sys.stdin.readlines()
+    sum = 0
+    answer = 0
+    table = {
+        0: 0
+    }
+    notFound = True
+    while(notFound):
+        for line in lines:
+            num = int(line)
+            sum += num
+            print("Sum: ", sum, "\tNum: ", num)
+            if not sum in table:
+                table[sum] = 1
+            else:
+                table[sum] = table[sum] + 1
+            if table[sum] >= 2:
+                answer = sum
+                notFound = False
+                break
+
+    print(answer)
+
+
+def part1():
+    sum = 0
+    for line in sys.stdin:
+        sum += int(line)
+
+    print(sum)
+
+def main():
+    part2()
+
+if __name__ == "__main__":
+    main()
---
+++ day1/run.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+cat input.txt | python3 Main.py
